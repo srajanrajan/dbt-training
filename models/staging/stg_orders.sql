c59ca05fac47cee0d625aa7b258baa5db81996cf
rajan5588@live.com <rajan5588@live.com>
2023-04-28 07:05:45 +0000

    Developed stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,17 @@
+select 
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    c.customername,
+    c.segment,
+    c.country,
+    p.category,
+    p.productname,
+    p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+    on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+    on o.productid = p.productid
